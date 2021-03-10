195999f145beaa0158bf281d80a3ce3233d39ec8
mungai-joel <mungaijoel123@gmail.com>
2021-03-10 22:07:54 +0300

    response api

--- start.sh
+++ start.sh
@@ -1,1 +1,9 @@
 python3.8 manage.py server
+
+
+# Application name	music
+# API key	5d9ca71a47d9dc1e5e1d5efcd507a769
+# Shared secret	0abd9588d40003cb80e8a1ddfe926630
+# Registered to	mungai-joel
+# http://ws.audioscrobbler.com/2.0
+# http://www.last.fm/api/auth/?api_key=xxx
